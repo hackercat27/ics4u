0bdccedb14d7cb65bc62ce27715ab71d86378628
hackercat <brentobi703@gmail.com>
2025-02-11 13:13:41 -0500

    java2 initial

---
+++ java2/src/Main.java
@@ -0,0 +1,24 @@
+
+/*
+ * Coding Task: Create a program in order to complete as many of the following tasks as you can:
+ * Within the main method:
+ * Print out each array using the Arrays.toString(int [] arr) method. (remember you’ll need to import Arrays)
+ * Print out information by accessing each method and print out information.
+ * OUTSIDE of the main method, create methods that:
+ *  - Create a method that:
+ *       - Randomly generates a number between 1-10 (which will be used to set the size of the array)
+ *       - Create an int array. Initialize the array to be the size of the random number between 1-10 that you just created.
+ *       - Randomly generate numbers between 10-20 and fill the array with these random numbers (Adjust the added value to our random number generator as modelled in Java 2C).
+ *  - Return true if the array has between 5-10 terms
+ *  - Return true if the first number is the smallest in the array (do you understand how to adjust your for loop?)
+ *  - Return true if the last number is the largest in the array (do you understand how to adjust your for loop?)
+ *  - Return the product of the first two values in the array
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
