589385ff5337bd92a9287200cbe23ef4addaae9e
hackercat <brentobi703@gmail.com>
2025-02-24 13:07:37 -0500

    outstanding changes

---
+++ java3/src/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Create a Program that:
+ *
+ * Stores ten 4-letter words that you have pre-set in an array (do not print these words out to the user)
+ * Randomly choose ONE of the words in the array to be the “secret word”
+ * Create two methods: startsWith(char a) and an endsWith(char b) that will check to see whether the String randomly chosen out of the array starts or ends with specified letters.
+ *         **Refer to some of the methods that already exist in the String class as they may be helpful - Java Library
+ * Create a method that determines whether or not they have correctly guessed the “secret word”
+ *
+ * Interact with a user so that they can type in values. You will want to prompt your user to do the following:
+ *
+ * Choose to play your game (once you’ve explained how the game works)
+ *
+ * Ask if they would like to do one of the following:
+ *
+ * Determine the starting letter
+ * Determine the ending letter
+ * Guess the word
+ * Exit the game (once they’ve guessed the word correctly, or no longer want to play)
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
